a155c74ec81bb6da61a8ce2779a3eaf16276eb6d
ClaudineiAn <claudinei2092016@gmail.com>
2025-08-30 15:56:03 +0100

    Done

--- api/data.js
+++ api/data.js
@@ -1,17 +1,11 @@
 // api/data.js
 import { generalController } from '../controllers/generalControllers.js';
 
-export default async function handler(req, res) {
+export default async function handler(request, response) {
   try {
     const data = await generalController.load();
-    res.status(200).json(data);
+    response.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    response.status(500).json({ error: error.message });
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
